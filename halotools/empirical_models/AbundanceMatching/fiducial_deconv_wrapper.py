642d048029ef3ce134033ff1b51c34e7100df41f
Andrew Hearin <andrew.hearin@yale.edu>
2015-09-17 17:06:53 -0400

    Modified call of convolved_fit from within fiducial_deconv_wrapper.fiducial_deconvolute so that it calls the cython-declared function. This is just step 1 of the wrapper alteration

--- halotools/empirical_models/AbundanceMatching/fiducial_deconv_wrapper.py
+++ halotools/empirical_models/AbundanceMatching/fiducial_deconv_wrapper.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import numpy.ctypeslib as C
 
+import fiducial_deconvolute_declarations
+
 # define types
 _double_ctype = C.ndpointer(np.float64, ndim=1, flags='C')
 
@@ -23,8 +25,10 @@ def fiducial_deconvolute(af_key, af_val, smm, mf, scatter, repeat=40, sm_step=0.
         offset = sm_step-sm_min
         af_key += offset
         smm += offset
-    _C_LIB.convolved_fit(af_key, af_val, len(af_key), smm, mf, len(mf), float(scatter), \
-        int(repeat), sm_step)
+    # _C_LIB.convolved_fit(af_key, af_val, len(af_key), 
+    #     smm, mf, len(mf), float(scatter), int(repeat), sm_step)
+    fiducial_deconvolute_declarations.convolved_fit(af_key, af_val, len(af_key), 
+        smm, mf, len(mf), float(scatter), int(repeat), sm_step)
     if sm_min <= 0:
         smm -= offset
         af_key -= offset
