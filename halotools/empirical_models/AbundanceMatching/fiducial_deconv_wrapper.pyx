63cc9526e94c37e8f62f28bb33d729bb009a4683
Andrew Hearin <andrew.hearin@yale.edu>
2015-09-17 17:17:04 -0400

    Deleted Yao's _C_LIB declarations

--- halotools/empirical_models/AbundanceMatching/fiducial_deconv_wrapper.pyx
+++ halotools/empirical_models/AbundanceMatching/fiducial_deconv_wrapper.pyx
@@ -5,17 +5,6 @@ import numpy.ctypeslib as C
 
 cimport fiducial_deconvolute_declarations
 
-# define types
-_double_ctype = C.ndpointer(np.float64, ndim=1, flags='C')
-
-# load the c library
-here = os.path.abspath(os.path.dirname(__file__))
-_C_LIB = C.load_library('fiducial_deconvolute', here)
-_C_LIB.convolved_fit.restype = None
-_C_LIB.convolved_fit.argtypes = [_double_ctype, _double_ctype, C.ctypes.c_int, \
-        _double_ctype, _double_ctype, C.ctypes.c_int, C.ctypes.c_double,\
-        C.ctypes.c_int, C.ctypes.c_double]
-
 def fiducial_deconvolute(af_key, af_val, smm, mf, scatter, repeat=40, sm_step=0.01):
     if len(smm) != len(mf):
         raise ValueError('`smf` and `mf` must have the same size!')
